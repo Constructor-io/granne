8ed2220cfb0a0d067b2bb1f979ab9b6d50ad828d
menshikh-iv <menshikh.iv@gmail.com>
2024-07-08 18:33:54 +0300

    upd

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,23 @@
+use core::mem;
+use std::alloc::{alloc, Layout};
 use std::io::{Result, Write};
+use std::ptr;
 
 /// Loads a slice of bytes as a slice of type T
 pub unsafe fn load_bytes_as<T>(bytes: &[u8]) -> &[T] {
-    let elements: &[T] =
-        ::std::slice::from_raw_parts(bytes.as_ptr() as *const T, bytes.len() / ::std::mem::size_of::<T>());
+    let curr_ptr = bytes.as_ptr();
+    let not_aligned = curr_ptr as usize % mem::align_of::<T>() != 0;
+
+    let ptr = if not_aligned {
+        let layout = Layout::from_size_align(bytes.len(), mem::align_of::<T>()).unwrap();
+        let new_ptr = alloc(layout);
+        ptr::copy_nonoverlapping(curr_ptr, new_ptr, bytes.len());
+        new_ptr
+    } else {
+        bytes.as_ptr()
+    };
+
+    let elements: &[T] = ::std::slice::from_raw_parts(ptr as *const T, bytes.len() / ::std::mem::size_of::<T>());
 
     elements
 }
