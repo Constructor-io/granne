4da3a356f0aa2e8b7c74280fd68c3ddfd6fd969c
Sergey Ivanychev <sergeyivanychev@gmail.com>
2024-08-29 17:55:33 +0200

    Also build for Python 3.8

--- build-macos-wheels.sh
+++ build-macos-wheels.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-rm -rf dist || true
+rm -rf macos_dist || true
 
 # Cross-platform way to create a temporary dir.
 mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
 pip wheel . -w $mytmpdir --no-deps
 
-delocate-wheel $mytmpdir/*macosx*.whl -w dist/
+delocate-wheel $mytmpdir/*macosx*.whl -w macos_dist/
