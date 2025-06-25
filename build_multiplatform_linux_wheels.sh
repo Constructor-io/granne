6369be498e049129b302c42db5d6dc1111e530db
Sergey Ivanychev <sergeyivanychev@gmail.com>
2025-06-25 12:12:09 +0200

    Support Python 3.12 and 3.13

--- build_multiplatform_linux_wheels.sh
+++ build_multiplatform_linux_wheels.sh
@@ -3,21 +3,20 @@ set -euxo pipefail
 
 echo "PWD ${PWD}"
 
-rm -rf dist || true
+rm -rf dist/linux || true
+rm -rf build
 
 # Cross-platform way to create a temporary dir.
 mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
-# Build x86 wheels, put in a temp folder.
-docker run --platform=linux/amd64 --rm -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 /io/build-wheels.sh
-mv dist "${mytmpdir}/dist_x86_64"
-
 # Build ARM wheels, put in a temp folder.
-docker run --platform=linux/arm64/v8 --rm -v `pwd`:/io quay.io/pypa/manylinux2014_aarch64 /io/build-wheels.sh
-mv dist "${mytmpdir}/dist_aarch64"
+docker run --platform=linux/arm64/v8 --rm -v `pwd`:/io \
+  -e OUTPUT_DIST=/io/dist/linux/aarch64 \
+  quay.io/pypa/manylinux_2_28_aarch64 /io/build-wheels.sh
+rm -rf build
 
-# Move all wheels to dist/
-mkdir dist
-mv ${mytmpdir}/dist_x86_64/* dist/
-mv ${mytmpdir}/dist_aarch64/* dist/
-rm -rf "${mytmpdir}/dist_x86_64" "${mytmpdir}/dist_aarch64"
+# Build x86 wheels, put in a temp folder.
+docker run --platform=linux/amd64 --rm -v `pwd`:/io \
+  -e OUTPUT_DIST=/io/dist/linux/amd64 \
+  quay.io/pypa/manylinux_2_28_x86_64 /io/build-wheels.sh
+rm -rf build
